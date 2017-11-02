1ce564ec13d98d0eb3d2f7944befef453a36e7c6
david mark <markkariuki17@gmail.com>
2017-11-02 11:40:53 +0300

    add readme

--- js/scripts.js
+++ js/scripts.js
@@ -10,5 +10,5 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 5000); // Change image every 2 seconds
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
