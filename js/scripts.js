c1e510d24bc3442b0cbb9989971a9ddfbbd284bf
david mark <markkariuki17@gmail.com>
2018-03-28 16:51:45 +0300

    add change

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
-var myIndex = 0;
-carousel();
+var myVar;
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+function myFunction() {
+    myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
 }
